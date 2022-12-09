82a6e95284ac6a1a2cf2dc4465c147c3dbf9baff
realtrynna <realtrynna@gmail.com>
2022-12-09 17:50:20 +0900

    Feature Peer 간 offer&answer와 Stream 송수신 기능 추가

--- src/socket.ts
+++ src/socket.ts
@@ -8,8 +8,7 @@ export default (server: any) => {
     io.on("connection", (socket) => {
         // 모든 이벤트 수신(미들웨어)
         socket.onAny((event) => {
-            console.log(io.sockets.adapter);
-        })
+        });
         // 방 입장
         socket.on("room", (title, done) => {
             const roomTitle = title;
@@ -37,5 +36,36 @@ export default (server: any) => {
             // 퇴장 메세지
             socket.rooms.forEach(room => socket.to(room).emit("leave"));
         })
+        //
+        //
+        //
+        // 피어 입장
+        socket.on("peerRoom", (data, done) => {
+            const peerRoomTitle = data;
+
+            socket.join(peerRoomTitle);
+
+            socket.to(peerRoomTitle).emit("peerRoomJoin", {
+                message: "사용자 입장",
+            });
+        });
+        // offer 수신
+        socket.on("offer", data => {
+            const { offer, roomTitle } = data;
+
+            socket.to(roomTitle).emit("offer", offer);
+        });
+        // answer 수신
+        socket.on("answer", data => {
+            const { answer, roomTitle } = data;
+
+            socket.to(roomTitle).emit("answer", answer);
+        });
+        // ice 수신
+        socket.on("ice", data => {
+            const { candidate, roomTitle } = data;
+
+            socket.to(roomTitle).emit("ice", candidate);
+        }) 
     });
 }
